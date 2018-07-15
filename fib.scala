302deff85ee2f7d1520b7f679704aeca7e4c9290
YuMuuu <c01150788f@edu.teu.ac.jp>
2018-07-16 03:03:40 +0900

    add fib.scala 関数型プログラミングの課題の奴

--- fib.scala
+++ fib.scala
@@ -1,1 +1,7 @@
-def fib(n:Int): Int = ???
+def fib(n:Int): Int = {
+  @annotation.tailrec
+  def loop(n: Int, a: Int, b: Int): Int =
+    if(n == 0) a
+    else loop(n - 1, b, a + b)
+  loop(n, 0, 1)
+}
