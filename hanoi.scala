80568fc369bb94c520af7817c0e47b99479542ea
YuMuuu <c01150788f@edu.teu.ac.jp>
2018-07-16 03:33:55 +0900

    change hanoi.scala うごくようにした

--- hanoi.scala
+++ hanoi.scala
@@ -1,1 +1,5 @@
-def hanoi(n:Int, from:Char, work:Char, dest:Char):Unit = ???
+def hanoi(n:Int, from:Char, work:Char, dest:Char):Unit = {
+  if(n >= 2) hanoi(n-1, from, dest, work) 
+  println(from + "から" + dest + "へ")
+  if(n >= 2) hanoi(n-1, work, from, dest)
+}
